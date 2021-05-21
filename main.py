2b7460e8971594adfe8d56e1966200dd15c793fd
chen-jh@outlook.com <chen-jh@outlook.com>
2021-05-21 11:26:34 +0800

    add code

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait as wWait
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
 
 if __name__ == '__main__':
     opts = Options()
@@ -9,5 +12,12 @@ if __name__ == '__main__':
     web.get('https://www.baidu.com')
     print(web.title)
     print(web.current_url)
-    print(web.session_id)
+    kw = web.find_element_by_xpath('//*[@id="kw"]')
+    kw.send_keys('python')
+    kw.submit()
+
+    first_x = '//*[@id="content_left"]/div/div/h3/a'
+    wWait(web, 10, 0.2).until(ec.presence_of_element_located((By.XPATH, first_x)))
+    first = web.find_element_by_xpath(first_x)
+    print(first.text)
     web.quit()

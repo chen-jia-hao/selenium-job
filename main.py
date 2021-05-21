6e738885395fc18ef209375602ad6c916d1b454d
chen-jh@outlook.com <chen-jh@outlook.com>
2021-05-21 11:35:25 +0800

    add code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait as wWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
+import time
 
 if __name__ == '__main__':
     opts = Options()
@@ -17,7 +18,9 @@ if __name__ == '__main__':
     kw.submit()
 
     first_x = '//*[@id="content_left"]/div/div/h3/a'
-    wWait(web, 10, 0.2).until(ec.presence_of_element_located((By.XPATH, first_x)))
+
+    # wWait(web, 10, 0.2).until(ec.presence_of_element_located((By.XPATH, first_x)))
+    time.sleep(10)
     first = web.find_element_by_xpath(first_x)
     print(first.text)
     web.quit()
